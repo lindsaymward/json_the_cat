66b33c7d44d3b007e8d719cd8975963786243b5b
Lindsay Ward <lward23@hotmail.com>
2023-05-11 15:11:54 -0700

    Add tests for valid and invalid breeds

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+const { fetchBreedDescription } = require('../breedFetcher');
+
+describe("fetchBreedDescription", () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns an error if an invalid breed is chosen, via callback', (done) => {
+    fetchBreedDescription('Fish', (err, desc) => {
+      assert.equal(desc, null);
+
+      const expectedError = `This breed cannot be found. Please try again. null`;
+
+      assert.equal(expectedError, err);
+
+      done();
+    });
+  });
+});
